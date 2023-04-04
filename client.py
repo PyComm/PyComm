8b0aaf58c98d38590d5137d7c0f4466cf402a42b
Cristian Bruna <cristian.bruna@sansnao.usm.cl>
2023-04-04 15:35:41 -0400

    Base for register logs. Only messages with date-time

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
 import socket
+import datetime
 
 # Definir host y puerto
 HOST = '127.0.0.1'
 PORT = 65431
+
+# Crear un archivo de log con fecha y hora
+log_filename = "logs/" + datetime.datetime.now().strftime("%Y-%m-%d") + ".log"
+log_file = open(log_filename, "w")
 
 # Crear un socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -11,6 +16,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         # Pedir al usuario que ingrese un mensaje
         message = input('Ingrese un mensaje: ')
+        
         # Codificar el mensaje utilizando el algoritmo César
         encoded_message = ''
         for char in message:
@@ -19,10 +25,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 encoded_message += shifted_char
             else:
                 encoded_message += char
+        
         # Enviar el mensaje al servidor
         s.sendall(encoded_message.encode())
+        
+        # Guardar el mensaje en el archivo de log con fecha y hora
+        timestamp = datetime.datetime.now().strftime("[%H:%M:%S]")
+        log_file.write(f"{timestamp} Cliente: {message}\n")
+
         # Esperar la respuesta del servidor
         data = s.recv(1024)
+
         # Decodificar la respuesta utilizando el algoritmo César
         decoded_response = ''
         for char in data.decode():
@@ -31,4 +44,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 decoded_response += shifted_char
             else:
                 decoded_response += char
+        
+        
+        
         print('Respuesta recibida del servidor:', decoded_response)
+        # Guardar el mensaje en el archivo de log con fecha y hora
+        timestamp = datetime.datetime.now().strftime("[%H:%M:%S]")
+        log_file.write(f"{timestamp} Servidor: {decoded_response}\n")
+        # log_file.write(f"{timestamp} Warning: No se logró realizar envío\n")
+
+# Cerrar el archivo de log
+log_file.close()
