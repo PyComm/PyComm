220c0dc519f70553f5482fe3d7cc156074cbf0dd
ROBERTO MIGUEL COVARRUBIAS FIGUEROA <roberto.covarrubias@sansano.usm.cl>
2023-04-08 11:58:38 -0400

    codificacion cesar mejorada y agregar codificacion a numeros

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import socket
+from unidecode import unidecode
 
 # Definir host y puerto
 HOST = '127.0.0.1'
-PORT = 65431
+PORT = 65435
 
 # Crear un socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -11,24 +12,41 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         # Pedir al usuario que ingrese un mensaje
         message = input('Ingrese un mensaje: ')
+        message = unidecode(message)
         # Codificar el mensaje utilizando el algoritmo César
         encoded_message = ''
         for char in message:
             if char.isalpha():
-                shifted_char = chr((ord(char) - ord('a') - 3) % 26 + ord('a'))
+                if char.isupper():
+                    shifted_char = chr((ord(char) - ord('A') - 3) % 26 + ord('a'))
+                else:
+                    shifted_char = chr((ord(char) - ord('a') - 3) % 26 + ord('A'))
                 encoded_message += shifted_char
+            elif char.isdigit():
+                shifted_char = (int(char)-5)%10
+                encoded_message += str(shifted_char)
             else:
                 encoded_message += char
         # Enviar el mensaje al servidor
-        s.sendall(encoded_message.encode())
+        s.sendall(encoded_message.encode('utf-8'))
         # Esperar la respuesta del servidor
-        data = s.recv(1024)
+        data = s.recv(1024).decode('utf-8')
         # Decodificar la respuesta utilizando el algoritmo César
         decoded_response = ''
-        for char in data.decode():
+        for char in data:
             if char.isalpha():
-                shifted_char = chr((ord(char) - ord('a') + 3) % 26 + ord('a'))
+                if char.isupper():
+                    shifted_char = chr((ord(char) - ord('A') + 3) % 26 + ord('a'))
+                else:
+                    shifted_char = chr((ord(char) - ord('a') + 3) % 26 + ord('A'))
                 decoded_response += shifted_char
+            elif char.isdigit():
+                if int(char) < 5:
+                    shifted_char = (int(char)+5)
+                else:
+                    shifted_char = int(char)-5
+                decoded_response += str(shifted_char)
             else:
                 decoded_response += char
+
         print('Respuesta recibida del servidor:', decoded_response)
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
+from unidecode import unidecode
 
 # Definir host y puerto
 HOST = '127.0.0.1'
-PORT = 65431
+PORT = 65435
 
 # Crear un socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -15,26 +16,42 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Conexión establecida desde', addr)
         while True:
             # Recibir el mensaje del cliente
-            data = conn.recv(1024)
+            data = conn.recv(1024).decode('utf-8')
             if not data:
                 break
             # Decodificar el mensaje utilizando el algoritmo César
             decoded_data = ''
-            for char in data.decode():
+            for char in data:
                 if char.isalpha():
-                    shifted_char = chr((ord(char) - ord('a') + 3) % 26 + ord('a'))
+                    if char.isupper():
+                        shifted_char = chr((ord(char) - ord('A') + 3) % 26 + ord('a'))
+                    else:
+                        shifted_char = chr((ord(char) - ord('a') + 3) % 26 + ord('A'))
                     decoded_data += shifted_char
+                elif char.isdigit():
+                    if int(char) < 5:
+                        shifted_char = (int(char)+5)
+                    else:
+                        shifted_char = (int(char)-5)
+                    decoded_data += str(shifted_char)
                 else:
                     decoded_data += char
             print('Mensaje recibido del cliente:', decoded_data)
             # Enviar una respuesta al cliente
             response = input('Ingrese una respuesta al cliente: ')
+            response = unidecode(response)
             # Codificar la respuesta utilizando el algoritmo César
             encoded_response = ''
             for char in response:
                 if char.isalpha():
-                    shifted_char = chr((ord(char) - ord('a') - 3) % 26 + ord('a'))
+                    if char.isupper():
+                        shifted_char = chr((ord(char) - ord('A') - 3) % 26 + ord('a'))
+                    else:
+                        shifted_char = chr((ord(char) - ord('a') - 3) % 26 + ord('A'))
                     encoded_response += shifted_char
+                elif char.isdigit():
+                    shifted_char = (int(char)-5)%10
+                    encoded_response += str(shifted_char)
                 else:
                     encoded_response += char
-            conn.sendall(encoded_response.encode())
+            conn.sendall(encoded_response.encode('utf-8'))
